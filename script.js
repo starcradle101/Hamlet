725e3c3878392a8569c328831ac4f64f12e04bff
Hoonie <starcradle@Hoonieui-MacBookPro.local>
2024-03-04 11:07:24 +0900

    Feat: Add todo filter feature 투두리스트 필터 기능 구현
    Related issue: #9

--- script.js
+++ script.js
@@ -5,31 +5,60 @@ const NEW_TODO_INPUT = document.getElementById('new-todo-input');
 const EDIT_TODO_INPUT = document.getElementById('edit-todo-input');
 const CANCEL_EDIT_BTN = document.getElementById('cancel-edit');
 const SAVE_TODO_BTN = document.getElementById('save-todo');
+const TODO_FILTER_ALL = document.querySelector('.all');
+const TODO_FILTER_FINISHED = document.querySelector('.finished');
+const TODO_FILTER_UNFINISHED = document.querySelector('.unfinished');
+let currentEditTodoItem = null;
+
+function generateUniqueId() {
+	return Date.now().toString(36);
+}
 
 function loadItemsFromLocalStorage() {
 	const todoItems = JSON.parse(localStorage.getItem('todoItems')) || [];
+	const fragment = document.createDocumentFragment();
 
 	todoItems.forEach((item) => {
-		const todoItem = createTodoItem(item.text);
-		TODO_CONTAINER.appendChild(todoItem);
+		const todoItem = createTodoItem(item.text, item.isComplete, item.id);
 		addTodoItemEventListeners(todoItem);
+		if (item.isComplete) {
+			todoItem.querySelector('.todo-text').style.textDecoration =
+				'line-through';
+			todoItem.querySelector('.checkbox').checked = true;
+		}
+		fragment.appendChild(todoItem);
 	});
+
+	TODO_CONTAINER.appendChild(fragment);
 }
 
-function saveItemInLocalStorage(text) {
+function saveItemInLocalStorage(todoItem, isComplete = false) {
 	const todoItems = JSON.parse(localStorage.getItem('todoItems')) || [];
-	const newItem = { text };
+	const newItem = {
+		id: todoItem.getAttribute('data-id'),
+		text: todoItem.querySelector('.todo-text').textContent,
+		isComplete,
+	};
 	todoItems.push(newItem);
 	localStorage.setItem('todoItems', JSON.stringify(todoItems));
 }
 
-function updateLocalStorage() {
-	const todoItems = [];
-	const todoTexts = document.querySelectorAll('.todo-text');
-	todoTexts.forEach((todo) => {
-		todoItems.push({ text: todo.textContent });
-	});
-	localStorage.setItem('todoItems', JSON.stringify(todoItems));
+function updateLocalStorage(itemId, newText) {
+	const todoItems = JSON.parse(localStorage.getItem('todoItems')) || [];
+	const itemIndex = todoItems.findIndex((item) => item.id === itemId);
+	if (itemIndex !== -1) {
+		todoItems[itemIndex].text = newText;
+		localStorage.setItem('todoItems', JSON.stringify(todoItems));
+	}
+}
+
+function updateItemStatus(itemId, isComplete) {
+	const todoItems = JSON.parse(localStorage.getItem('todoItems')) || [];
+	const itemIndex = todoItems.findIndex((item) => item.id === itemId);
+	if (itemIndex !== -1) {
+		todoItems[itemIndex].isComplete = isComplete;
+		localStorage.setItem('todoItems', JSON.stringify(todoItems));
+	}
 }
 
 function addTodoItemEventListeners(todoItem) {
@@ -37,27 +66,34 @@ function addTodoItemEventListeners(todoItem) {
 	deleteBtn.addEventListener('click', deleteTodoItem);
 
 	const editBtn = todoItem.querySelector('.edit-todo');
-	editBtn.addEventListener('click', () => editTodo(todoItem));
+	editBtn.addEventListener('click', () => editTodoItem(todoItem));
+
+	const checkbox = todoItem.querySelector('.checkbox');
+	checkbox.addEventListener('change', () => {
+		const itemId = todoItem.getAttribute('data-id');
+		const isComplete = checkbox.checked;
+		updateItemStatus(itemId, isComplete);
+
+		// 체크박스 상태에 따라 스타일 업데이트
+		if (isComplete) {
+			todoItem.querySelector('.todo-text').style.textDecoration =
+				'line-through';
+		} else {
+			todoItem.querySelector('.todo-text').style.textDecoration = 'none';
+		}
+	});
 }
 
-function addTodo() {
-	const todoText = NEW_TODO_INPUT.value.trim();
-	if (todoText !== '') {
-		const todoItem = createTodoItem(todoText);
-		TODO_CONTAINER.insertBefore(todoItem);
-		NEW_TODO_INPUT.value = '';
-	}
-}
-
-function createTodoItem(text) {
+function createTodoItem(text, isComplete = false, id) {
 	const todoItem = document.createElement('div');
 	todoItem.classList.add('todo-item');
+	todoItem.setAttribute('data-id', id || generateUniqueId());
 	todoItem.innerHTML = `
-        <p class="todo-text">${text}</p>
-        <input type="checkbox" class="checkbox" />
-        <button class="edit-todo">Edit</button>
-        <button class="delete-todo">Delete</button>
-    `;
+  <p class="todo-text">${text}</p>
+  <input type="checkbox" class="checkbox" ${isComplete ? 'checked' : ''} />
+  <button class="edit-todo">Edit</button>
+  <button class="delete-todo">Delete</button>
+  `;
 	return todoItem;
 }
 
@@ -71,7 +107,23 @@ function editTodo(todoItem) {
 
 function deleteTodoItem() {
 	this.parentElement.remove();
-	updateLocalStorage();
+	const itemId = this.parentElement.getAttribute('data-id');
+	deleteItemFromLocalStorage(itemId);
+}
+
+function deleteItemFromLocalStorage(itemId) {
+	const todoItems = JSON.parse(localStorage.getItem('todoItems')) || [];
+	const updatedTodoItems = todoItems.filter((item) => item.id !== itemId);
+	localStorage.setItem('todoItems', JSON.stringify(updatedTodoItems));
+}
+
+function alertInput() {
+	let input = document.getElementById('new-todo-input').value;
+	if (input.trim() === '') {
+		alert('빈 값은 입력될 수 없습니다!');
+		return false;
+	}
+	return true;
 }
 
 function hideModal() {
@@ -81,31 +133,50 @@ function hideModal() {
 
 function saveTodoItem() {
 	const editedTodoText = EDIT_TODO_INPUT.value.trim();
-	if (editedTodoText !== '') {
-		currentEditTodoItem.querySelector('.todo-text').textContent =
-			editedTodoText;
-		updateLocalStorage();
-		hideModal();
+	if (!editedTodoText) {
+		alert('수정할 내용을 입력하세요!');
+		return;
 	}
+	const itemId = currentEditTodoItem.getAttribute('data-id');
+	currentEditTodoItem.querySelector('.todo-text').textContent = editedTodoText;
+	updateLocalStorage(itemId, editedTodoText);
+	hideModal();
+}
+
+function filterTodoItems(event) {
+	const filterType = event.target.className;
+	const todoItems = TODO_CONTAINER.querySelectorAll('.todo-item');
+	todoItems.forEach((todoItem) => {
+		const isComplete = todoItem.querySelector('.checkbox').checked;
+		let displayStyle = 'flex';
+		if (
+			(filterType === 'finished' && !isComplete) ||
+			(filterType === 'unfinished' && isComplete)
+		) {
+			displayStyle = 'none';
+		}
+		todoItem.style.display = displayStyle;
+	});
 }
 
 function initialize() {
 	loadItemsFromLocalStorage();
-
+	TODO_FILTER_ALL.addEventListener('click', filterTodoItems);
+	TODO_FILTER_FINISHED.addEventListener('click', filterTodoItems);
+	TODO_FILTER_UNFINISHED.addEventListener('click', filterTodoItems);
 	ADD_TODO_BTN.addEventListener('click', (e) => {
 		e.preventDefault();
-		const todoText = NEW_TODO_INPUT.value.trim();
-		if (todoText !== '') {
-			const todoItem = createTodoItem(todoText);
-			TODO_CONTAINER.appendChild(todoItem);
-			addTodoItemEventListeners(todoItem);
-			saveItemInLocalStorage(todoText);
-			NEW_TODO_INPUT.value = '';
+		if (!alertInput()) {
+			return;
 		}
+		const todoText = NEW_TODO_INPUT.value.trim();
+		const todoItem = createTodoItem(todoText);
+		TODO_CONTAINER.appendChild(todoItem);
+		addTodoItemEventListeners(todoItem);
+		saveItemInLocalStorage(todoItem);
+		NEW_TODO_INPUT.value = '';
 	});
-
 	CANCEL_EDIT_BTN.addEventListener('click', hideModal);
-
 	SAVE_TODO_BTN.addEventListener('click', saveTodoItem);
 }
 
